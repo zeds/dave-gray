23f9a68b6a9498123b285c4e127a8ebeeb03c10e
Tsutomu Okumura <tom.zed39@gmail.com>
2023-01-16 09:46:51 +0900

    08 App.jsx

--- 06_lesson-rtk-query/src/App.jsx
+++ 06_lesson-rtk-query/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import TodoList from './features/todos/TodoList'
 import ProductList from './features/products/ProductList'
 
 function App() {
