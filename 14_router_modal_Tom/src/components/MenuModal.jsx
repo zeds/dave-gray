8375398ebe3174ece7920bd8c7b37063498f646f
Tsutomu Okumura <tom.zed39@gmail.com>
2023-02-03 09:55:51 +0900

    14_

--- 14_router_modal_Tom/src/components/MenuModal.jsx
+++ 14_router_modal_Tom/src/components/MenuModal.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import style from './MenuModal.module.scss'
 import { openModal } from '../features/modal/modalSlice'
+import { Icon } from '@iconify/react';
 
 export const MenuModal = ({
 	open
@@ -18,11 +19,12 @@ export const MenuModal = ({
 		<div onClick={(event)=> {
 			clickClose(event);
 		}} className={style.container}>
-			<div onClick={(event) => {
-				event.stopPropagation()
-			}}
-				className={style.menu}>
-				<button onClick={()=>clickClose()}>close</button>
+			<div onClick={(event) => {event.stopPropagation()}} className={style.menu}>
+				<a className={style.btn} onClick={()=>clickClose()}><Icon icon="mdi:close-box-outline" width="50" /></a>
+				<div>
+					<p><Icon icon="mdi:company" width="50" />会社概要</p>
+					<p>お問い合わせ</p>
+				</div>
 			</div>
 		</div>
 	)
