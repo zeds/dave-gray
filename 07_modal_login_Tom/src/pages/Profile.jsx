dd7d5c7099d362af13428fa25b907e72622ad0ea
Tsutomu Okumura <tom.zed39@gmail.com>
2023-02-28 22:20:45 +0900

    07_modal_login_Tom before eslint fix

--- 07_modal_login_Tom/src/pages/Profile.jsx
+++ 07_modal_login_Tom/src/pages/Profile.jsx
@@ -35,6 +35,35 @@ import {
 ・loginしたら、state.user, state.jwtが更新されている
 ・
 
+[useState]
+　https://beta.reactjs.org/reference/react/useState
+・In Strict Mode, React will call your initializer function twice
+　This is development-only behavior and does not affect production.
+・set function only updates the state variable for the next render
+・If you read the state variable after calling the set function, you will still get the old value
+・If the new value you provide is identical to the current state, as determined by an Object.is comparison,
+　React will skip re-rendering the component and its children.
+✅useStateの注意点
+　https://tyotto-good.com/react/usestate-pitfalls
+・useStateはトップレベルで呼び出す。条件分岐内で呼び出してはいけない。Reactがhookの順序がわからなくなってしまう。
+・Hooksのルールを強制できる、eslint-plugin-react-hooks
+　https://www.npmjs.com/package/eslint-plugin-react-hooks
+　　
+
+[RTK query]
+　https://redux-toolkit.js.org/rtk-query/usage/queries
+・isSuccessが2回呼ばれる？
+　原因：index.jsxに<React.StrictMode>を設定しているため。
+・openというstateを作り、buttonでtrue/falseを設定すると、useEffectも反応して
+　APIが呼び出されてしまった。なぜ？？ただし、strapiのAPIは呼び出されていなかった。
+　cachingだけ見ているようだ。
+　https://dev.to/raaynaldo/rtk-query-tutorial-crud-51hl
+
+
+・RTK queryを使うと、isLoading、isSuccessなどのstate管理をしなくて済む
+・caching：なぜcachingが必要か？
+　データーを2回目にダウンロードするとき、updateされてる時だけにしたい。
+　
 
  */
 
@@ -45,6 +74,7 @@ export const Profile = () => {
   let user = useSelector(selectCurrentUser);
   let token = useSelector(selectCurrentToken);
   const [cookies, setCookie, removeCookie] = useCookies(["cookie-name"]);
+  const [open, setOpen] = useState(false);
 
   const obj = {
     background: "/src/assets/surf-small.jpg",
@@ -62,15 +92,25 @@ export const Profile = () => {
   const {
     data: response,
     isLoading,
+    isFetching,
     isSuccess,
     isError,
     error,
   } = useGetMeQuery(cookies.jwt);
   //} = useGetMoviesQuery();
 
-  const test = () => {
-    setIsAvatar(true);
-  };
+  if (isLoading) {
+    console.log("isLoading");
+    return <div>Loading...</div>;
+  }
+  if (isFetching) {
+    console.log("isFetching");
+    return <div>Fetching...</div>;
+  }
+  if (isError) {
+    console.log({ error });
+    return <div>{error.status}</div>;
+  }
 
   if (isSuccess) {
     console.log("isSuccess response=", response);
@@ -82,15 +122,26 @@ export const Profile = () => {
 
     obj.avatar = "https://lusty.asia:1443" + response.avatar_url;
     console.log("obj.avatar=", obj.avatar);
-    test();
     //dispatch(logOut);
+    if (open === false) {
+      setOpen(true);
+    }
   }
 
   if (isError) {
     return <>API呼び出しで失敗しました</>;
   }
 
-  return <>{isAvatar ? <PublicProfile props={obj} /> : <div>Loading</div>}</>;
+  const toggle = () => setOpen(!open);
+
+  return (
+    <>
+      <button onClick={toggle}>{open ? "close" : "open"}</button>
+      <div className={open ? style.isOpen : style.isClose}>
+        <PublicProfile props={obj} />
+      </div>
+    </>
+  );
 
   const welcome = user ? `Profile ${user.username}!` : "Welcome!";
   const tokenAbbr = `${token.slice(0, 9)}...`;
